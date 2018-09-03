b9d6ed4a249e46b961ba693d5e333625095f5a9d
Ewen <wang.yuxu@husky.neu.edu>
2018-09-03 11:32:43 -0400

    auth

--- auth/auth.settings.js
+++ auth/auth.settings.js
@@ -13,7 +13,7 @@ module.exports = {
      *
      * If TRUE, authorization files will be imported and checked
      */
-    enabled: false, // [true, false] Default: TRUE.
+    enabled: true, // [true, false] Default: TRUE.
 
     /**
      * ## mode
