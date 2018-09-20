91f9b3b89e7b34925d0d7313c4598bad8b389968
Ewen <wang.yuxu@husky.neu.edu>
2018-09-19 20:02:38 -0400

    saving path

--- levels/hawkdove-interactive/game/client_types/logic.js
+++ levels/hawkdove-interactive/game/client_types/logic.js
@@ -141,7 +141,7 @@ module.exports = function (treatmentName, settings, stager, setup, gameRoom) {
 
     stager.extendStep('endSurvey', {
         cb: function () {
-            var path = channel.getGameDir() + 'experiments/data_' + node.nodename + '.json';
+            var path = channel.getGameDir() + 'experiments/data_' + channel.session + '.json';
             console.log("Saving game data to " + path);
             fs.writeFile(path, JSON.stringify(node.game.gameData, null, 2), function (err) {
                 if (err) {
